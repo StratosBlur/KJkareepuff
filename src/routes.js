c7527fe574876140505e73cf3b3e84ad985142f2
CuteCoder <wave_onlyone@hotmail.com>
2018-12-24 00:06:23 +0700

    Initialize Project

---
+++ src/routes.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Home from './features/home'
+import { Route, BrowserRouter } from 'react-router-dom';
+
+const Routes = () => (
+    <BrowserRouter>
+      <Route path="/" component={Home} />
+    </BrowserRouter>
+)
+
+export default Routes;
