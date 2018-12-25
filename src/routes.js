51cd16e4a4571c3b9d357c73cb152df4fd363434
Jarunchairat Wosonthia <jarunchairat.w@ku.th>
2018-12-25 15:25:34 +0700

    first commit

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import { Route, BrowserRouter, Switch } from 'react-router-dom';
 import Home from './features/home'
 import Story from './features/story'
-import { Route, BrowserRouter, Switch } from 'react-router-dom';
+import Register from './features/register';
 
 const Routes = () => (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path = "/story" component={Story} />
+        <Route path = "/register" component={Register} />
       </Switch>
     </BrowserRouter>
 )
