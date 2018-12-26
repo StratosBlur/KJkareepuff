c876445dd417b26fe02fdb97961a57627afe4789
CuteCoder <wave_onlyone@hotmail.com>
2018-12-26 23:26:04 +0700

    Fix merge conflict

--- src/features/order.js
+++ src/features/order.js
@@ -3,7 +3,7 @@ import OrderModal from './orderModal'
 import { Link, animateScroll as scroll, Element } from "react-scroll";
 
 export default class Order extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props)
         this.state = {
             orderedBox: 1,
@@ -16,10 +16,10 @@ export default class Order extends React.Component {
             numBox: 0,
             boxStyle: 1,
             orderList: [],
-          OrderProductTemplete:{
-              size: 0,
-              OrderAddress: "",
-          }
+            OrderProductTemplete: {
+                size: 0,
+                OrderAddress: "",
+            }
         }
         this.handleChangeAddflavor = this.handleChangeAddflavor.bind(this);
         this.handleChangeRemoveflavor = this.handleChangeRemoveflavor.bind(this);
@@ -29,7 +29,7 @@ export default class Order extends React.Component {
         this.handlePayment = this.handlePayment.bind(this);
     }
 
-    
+
 
     handleChange = (event) => {
         const newOrderProduct = this.state.OrderProductTemplete;
@@ -39,82 +39,85 @@ export default class Order extends React.Component {
                 size: event,
             }
         })
-        console.log("ordered product"+ this.state.OrderProduct)
     }
 
-    handleChangeStyle(style){
+    handleChangeStyle(style) {
         const originalStyle = this.state[style];
         this.setState({
-           boxStyle: originalStyle,
+            boxStyle: originalStyle,
         })
     }
 
     handleChangeAddflavor(flavor) {
-        console.log('Change flavor ', flavor)
-       const originalNumFlavor = this.state[flavor];
-       this.setState({
-          [flavor]: originalNumFlavor+1,
-       })
-       console.log('')
+        console.log("Size", this.state.OrderProductTemplete.size)
+        console.log('Flavor', this.state[flavor]);
+        if (this.state.OrderProductTemplete.size <= this.state[flavor]) {
+            const originalNumFlavor = this.state[flavor];
+            this.setState({
+                [flavor]: originalNumFlavor + 1,
+            })
+        }
+
     }
 
     handleChangeRemoveflavor(flavor) {
         const originalNumFlavor = this.state[flavor];
         this.setState({
-           [flavor]: originalNumFlavor-1,
+            [flavor]: originalNumFlavor - 1,
         })
-     }
+    }
 
-     handleChangeAddNumberOfBox() {
+    handleChangeAddNumberOfBox() {
+
         const originalNumBox = this.state.numBox;
         this.setState({
-           numBox: originalNumBox+1,
+            numBox: originalNumBox + 1,
         })
-     }
 
-     handleChangeRemoveNumberOfBox() {
+    }
+
+    handleChangeRemoveNumberOfBox() {
         const originalNumBox = this.state.numBox;
         this.setState({
-           numBox: originalNumBox-1,
+            numBox: originalNumBox - 1,
         })
-     }
+    }
 
-     handleAddToCart(){
+    handleAddToCart() {
         const originalOrderList = this.state.orderList
         originalOrderList.push({
-                orderedBox: this.state.orderedBox,
-                chicken: this.state.chicken,
-                pumpkin: this.state.pumpkin,
-                avocado: this.state.avocado,
-                gingko: this.state.gingko,
-                ngadum: this.state.ngadum,
-                banana: this.state.banana,
-                numBox: this.state.numBox,
-                boxStyle: this.state.boxStyle,
-                OrderProductTemplete: this.state.OrderProductTemplete
-            }
+            orderedBox: this.state.orderedBox,
+            chicken: this.state.chicken,
+            pumpkin: this.state.pumpkin,
+            avocado: this.state.avocado,
+            gingko: this.state.gingko,
+            ngadum: this.state.ngadum,
+            banana: this.state.banana,
+            numBox: this.state.numBox,
+            boxStyle: this.state.boxStyle,
+            OrderProductTemplete: this.state.OrderProductTemplete
+        }
         )
         this.setState({
             orderList: originalOrderList
         })
-        console.log("order ", this.state.orderList)
         window.sessionStorage.setItem("orderList", JSON.stringify(this.state.orderList));
-     }
+    }
 
     handlePayment() {
         const originalOrderList = this.state.orderList
         originalOrderList.push({
-                orderedBox: this.state.orderedBox,
-                chicken: this.state.chicken,
-                pumpkin: this.state.pumpkin,
-                avocado: this.state.avocado,
-                gingko: this.state.gingko,
-                ngadum: this.state.ngadum,
-                banana: this.state.banana,
-                numBox: this.state.numBox,
-                boxStyle: this.state.boxStyle,
-                OrderProductTemplete: this.state.OrderProductTemplete
-            }
+            orderedBox: this.state.orderedBox,
+            chicken: this.state.chicken,
+            pumpkin: this.state.pumpkin,
+            avocado: this.state.avocado,
+            gingko: this.state.gingko,
+            ngadum: this.state.ngadum,
+            banana: this.state.banana,
+            numBox: this.state.numBox,
+            boxStyle: this.state.boxStyle,
+            OrderProductTemplete: this.state.OrderProductTemplete
+        }
         )
         this.setState({
             orderList: originalOrderList
@@ -123,56 +126,37 @@ export default class Order extends React.Component {
         window.sessionStorage.setItem("orderList", JSON.stringify(this.state.orderList));
     }
 
-  render() {
-    return (
-        <div className="container">
-            <h2>เลือกขนาดที่คุณต้องการ</h2>
-            <br />
-            <div className="row">
-                <div className="col-lg-4 col-md-4 col-sm-4 product-preview">
-                <Link to="flavor"><img className="img-box-select" src="https://scontent.fbkk7-2.fna.fbcdn.net/v/t1.15752-9/48940554_1195298543952014_3681802818336849920_n.png?_nc_cat=109&_nc_ht=scontent.fbkk7-2.fna&oh=be0dba04e6582f587345b338450f886e&oe=5C8FDB97" 
-                        name="orderedBox" data-toggle="collapse" data-target="#choose-flavor" onClick={() => this.handleChange(1)}/>
-                        </Link> 
-                </div>   
-   
-                <div className="col-lg-4 col-md-4 col-sm-4 product-preview">
-                <Link to="flavor"><img className="img-box-select" src="https://scontent.fbkk7-2.fna.fbcdn.net/v/t1.15752-9/48412146_346510252741800_8548180281452920832_n.png?_nc_cat=109&_nc_ht=scontent.fbkk7-2.fna&oh=545309a97bf9e170ab5fd78723660b2d&oe=5C9381E8" 
-                        name="orderedBox" data-toggle="collapse" data-target="#choose-flavor" onClick={() => this.handleChange(2)}/>
-                    </Link>
-                </div>   
+    render() {
+        return (
+            <div className="container">
+                <h2>เลือกขนาดที่คุณต้องการ</h2>
+                <br />
+                <div className="row">
+                    <div className="col-lg-4 col-md-4 col-sm-4 product-preview">
+                        <Link to="flavor"><img className="img-box-select" src="https://scontent.fbkk7-2.fna.fbcdn.net/v/t1.15752-9/48940554_1195298543952014_3681802818336849920_n.png?_nc_cat=109&_nc_ht=scontent.fbkk7-2.fna&oh=be0dba04e6582f587345b338450f886e&oe=5C8FDB97"
+                            name="orderedBox" data-toggle="collapse" data-target="#choose-flavor" onClick={() => this.handleChange(1)} />
+                        </Link>
+                    </div>
 
-                <div className="col-lg-4 col-md-4 col-sm-4 product-preview">
-                   <Link to="flavor">
-                    <img className="img-box-select" src="https://scontent.fbkk7-2.fna.fbcdn.net/v/t1.15752-9/48417776_525187834651070_488155984179494912_n.png?_nc_cat=102&_nc_ht=scontent.fbkk7-2.fna&oh=f95c880baa2c477adabd555c079d9b30&oe=5CA208EF" 
-                        name="orderedBox"  data-toggle="collapse" data-target="#choose-flavor" onClick={() => this.handleChange(5)}/>
-                </Link>
-                </div>  
+                    <div className="col-lg-4 col-md-4 col-sm-4 product-preview">
+                        <Link to="flavor"><img className="img-box-select" src="https://scontent.fbkk7-2.fna.fbcdn.net/v/t1.15752-9/48412146_346510252741800_8548180281452920832_n.png?_nc_cat=109&_nc_ht=scontent.fbkk7-2.fna&oh=545309a97bf9e170ab5fd78723660b2d&oe=5C9381E8"
+                            name="orderedBox" data-toggle="collapse" data-target="#choose-flavor" onClick={() => this.handleChange(2)} />
+                        </Link>
+                    </div>
 
-                <div className="col-lg-4 col-md-4 col-sm-4 product-preview">
-                <Link to="flavor" activeClass="active" smooth={true} duration={500}>
-                    <img className="img-box-select" src="https://scontent.fbkk7-3.fna.fbcdn.net/v/t1.15752-9/48415180_367564324046203_4758985198903754752_n.png?_nc_cat=107&_nc_ht=scontent.fbkk7-3.fna&oh=39b9ea912a59831297aab70b9f4a865c&oe=5C90B2E9" 
-                        name="orderedBox" data-toggle="collapse" data-target="#choose-flavor" onClick={() => this.handleChange(8)}/>
-                </Link>
-                </div>  
+                    <div className="col-lg-4 col-md-4 col-sm-4 product-preview">
+                        <Link to="flavor">
+                            <img className="img-box-select" src="https://scontent.fbkk7-2.fna.fbcdn.net/v/t1.15752-9/48417776_525187834651070_488155984179494912_n.png?_nc_cat=102&_nc_ht=scontent.fbkk7-2.fna&oh=f95c880baa2c477adabd555c079d9b30&oe=5CA208EF"
+                                name="orderedBox" data-toggle="collapse" data-target="#choose-flavor" onClick={() => this.handleChange(5)} />
+                        </Link>
+                    </div>
 
-            </div>
-            
-            <div className="collapse" id="choose-flavor" >
-                            <OrderModal  
-                                handleChangeAddflavor={this.handleChangeAddflavor}
-                                handleChangeRemoveflavor={this.handleChangeRemoveflavor}
-                                handleChangeAddNumberOfBox={this.handleChangeAddNumberOfBox}
-                                handleChangeRemoveNumberOfBox={this.handleChangeRemoveNumberOfBox}
-                                handleChangeStyle={this.handleChangeStyle}
-                                orderedBox={this.state.orderedBox}
-                                chicken={this.state.chicken}
-                                pumpkin={this.state.pumpkin}
-                                avocado={this.state.avocado}
-                                gingko={this.state.gingko}
-                                ngadum={this.state.ngadum}
-                                banana={this.state.banana}
-                                numBox={this.state.numBox}
-                                boxStyle={this.state.boxStyle}
+                    <div className="col-lg-4 col-md-4 col-sm-4 product-preview">
+                        <Link to="flavor" activeClass="active" smooth={true} duration={500}>
+                            <img className="img-box-select" src="https://scontent.fbkk7-3.fna.fbcdn.net/v/t1.15752-9/48415180_367564324046203_4758985198903754752_n.png?_nc_cat=107&_nc_ht=scontent.fbkk7-3.fna&oh=39b9ea912a59831297aab70b9f4a865c&oe=5C90B2E9"
+                                name="orderedBox" data-toggle="collapse" data-target="#choose-flavor" onClick={() => this.handleChange(8)} />
+                        </Link>
+                    </div>
 
                             />
                         <a href="/ชำระเงิน" ><button className="btn btn-info" onClick={() => this.handlePayment()}>ชำระเงิน</button></a>
@@ -182,21 +166,45 @@ export default class Order extends React.Component {
             <Element name="flavor">
             </Element>
 
-    <div className="modal fade" id="cart" role="dialog">
-        <div className="modal-dialog modal-sm modal-dialog-centered">
-            <div className="modal-content">
-              <div className="modal-body">
-                <p>บรรจุสินค้าลงตะกร้าเรียบร้อยค่ะ</p>
-              </div>
-              <div class="modal-footer">
-                <button type="button" className="btn btn-default button-booking" data-dismiss="modal">Close</button>
-            </div>
-            </div>
-        </div>
-        </div>
+                <div className="collapse" id="choose-flavor" >
+                    <OrderModal
+                        handleChangeAddflavor={this.handleChangeAddflavor}
+                        handleChangeRemoveflavor={this.handleChangeRemoveflavor}
+                        handleChangeAddNumberOfBox={this.handleChangeAddNumberOfBox}
+                        handleChangeRemoveNumberOfBox={this.handleChangeRemoveNumberOfBox}
+                        handleChangeStyle={this.handleChangeStyle}
+                        orderedBox={this.state.orderedBox}
+                        chicken={this.state.chicken}
+                        pumpkin={this.state.pumpkin}
+                        avocado={this.state.avocado}
+                        gingko={this.state.gingko}
+                        ngadum={this.state.ngadum}
+                        banana={this.state.banana}
+                        numBox={this.state.numBox}
+                        boxStyle={this.state.boxStyle}
 
-        </div>
+                    />
+                    <a href="/payment" ><button className="btn btn-info" onClick={() => this.handlePayment()}>ชำระเงิน</button></a>
+                    <button type="button" className="btn btn-success" data-toggle="modal" data-target="#cart " onClick={() => this.handleAddToCart()}>ใส่ตะกร้า</button>
+
+                </div>
+                <Element name="flavor">
+                </Element>
+
+                <div className="modal fade" id="cart" role="dialog">
+                    <div className="modal-dialog modal-sm modal-dialog-centered">
+                        <div className="modal-content">
+                            <div className="modal-body">
+                                <p>บรรจุสินค้าลงตะกร้าเรียบร้อยค่ะ</p>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" className="btn btn-default button-booking" data-dismiss="modal">Close</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
         )
     }
-  }
-  
+}
